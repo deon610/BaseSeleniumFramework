1517c7f4bc2b05a4cb2324114ee97081643e00ac
Deon Dsouza <34719316+deon610@users.noreply.github.com>
2020-11-21 04:16:37 +0530

    commented polling time

--- src/main/java/designBase/BasePage.java
+++ src/main/java/designBase/BasePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 public class BasePage
 {
 	private static final int TIMEOUT = 120;
-	private static final int POLLING = 100;
+	//private static final int POLLING = 100;
 
 	protected WebDriver driver;
 	public BasePage(WebDriver driver)
